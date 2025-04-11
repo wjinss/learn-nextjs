40003d48680cc5f39dafa33226cf4d71c2624a11
wjinss <aastu4343@gmail.com>
2025-04-12 03:44:02 +0900

    #4.3 Movie Trailers

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,13 +1,30 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
   //   await new Promise((resolve) => setTimeout(resolve, 5000));
-  console.log(`Fetching movies: ${Date.now()}`);
+  // console.log(`Fetching movies: ${Date.now()}`);
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        className={styles.poster}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h2 className={styles.title}>{movie.title}</h2>
+        <h3>⭐️{movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
